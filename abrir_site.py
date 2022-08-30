e51acc0eff9a724fabe7ff1a26fab7ea83a6d6ff
Kaio Silva <jkaio37@hotmail.com>
2022-08-30 17:19:59 -0400

    Atualização Final de Semana

--- abrir_site.py
+++ abrir_site.py
@@ -17,18 +17,31 @@ url_ebc = 'https://radios.ebc.com.br/aovivo?emissora=radio-nacional-fm-brasilia'
 Hora = Hora_Semana
 Hora_FS = Hora_Fim_Semana
 
-Dia_util_6h00 = '05, 59, 30'
-Dia_util_6h25 = '06, 25, 30'
-Dia_util_6h26 = '06, 26, 00'
-Dia_util_6h55 = '06, 55, 30'
-Dia_util_11h00 = '10, 59, 30'
-Dia_util_11h25 = '11, 25, 00'
-Dia_util_11h25_10 = '11, 25, 10'
-Dia_util_05h00 = '05, 00, 30'
-Dia_FS_06h00 = '6, 59, '
+Dia_util_6h00 = '05, 59, 30' # Abrir Eleitoral
+Dia_util_6h25 = '06, 25, 30' # Fechar Eleitoral
+Dia_util_6h26 = '06, 26, 00' # Abrir EBC
+Dia_util_6h55 = '06, 55, 30' # Fecha EBC
+Dia_util_11h00 = '10, 59, 30' # Abrir Eleitoral
+Dia_util_11h25 = '11, 25, 00' # Fechar Eleitoral
+Dia_util_11h25_10 = '11, 25, 10' # Abrir EBC
+Dia_util_05h00 = '05, 00, 30' # Fecha EBC
+Dia_FS_05h59 = '6, 05, 59, 00' # Fecha EBC
+Dia_FS_06h00 = '6, 05, 59, 30' # Abrir Eleitoral
+Dia_FS_11h25 = '6, 11, 25, 30' # Fechar Eleitoral
+Dia_FS_02h00 = '0, 01, 59, 00' # Abrir EBC
 
 def abrir_fechar_site():
-    if Dia_util_6h00 == Hora():
+    if Dia_FS_05h59 == Hora_FS():
+        os.system('taskkill /im firefox.exe')
+    elif Dia_FS_06h00 == Hora_FS():
+        webbrowser.open_new(url_cabeca_de_rede)
+    elif Dia_FS_11h25 == Hora_FS():
+        os.system('taskkill /im firefox.exe')
+    elif Dia_FS_02h00 == Hora_FS():
+        webbrowser.open(url_ebc)
+    elif Dia_util_05h00 == Hora():
+        os.system('taskkill /im firefox.exe')
+    elif Dia_util_6h00 == Hora():
         webbrowser.open_new(url_cabeca_de_rede)
     elif Dia_util_6h25 == Hora():
         os.system('taskkill /im firefox.exe')
@@ -42,11 +55,9 @@ def abrir_fechar_site():
         os.system('taskkill /im firefox.exe')
     elif Dia_util_11h25_10 == Hora():
         webbrowser.open(url_ebc)
-    elif Dia_util_05h00 == Hora():
-        os.system('taskkill /im firefox.exe')
     elif Hora() == Hora():
         os.system('cls')
-        print('-------Automatic Tassio Time-------')
+        print('-------Automatic Time-------')
         print('Hora Local')
         print(Hora())
         print('Rádio Marinha Pantanal 105,9FM')
